e58f6072b6ed6c4b3c9c151403ef9b8e1fa17f4d
AykutSarac <aykutsarac0@gmail.com>
2021-06-29 00:25:34 +0300

    Removed contact

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -8,7 +8,6 @@ const Navbar = () => {
                 <li><a href="/">Home</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#stack">Stack</a></li>
-                <li><a href="#contact">Contact</a></li>
                 <li><a href="https://github.com/AykutSarac">GitHub</a></li>
             </ul>
         </nav>
