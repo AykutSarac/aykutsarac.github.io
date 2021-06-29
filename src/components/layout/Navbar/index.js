a55c29c7fbc135168b5bec4cce362da1c9663bc6
AykutSarac <aykutsarac0@gmail.com>
2021-06-29 11:45:04 +0300

    Performance improvements

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -1,15 +1,25 @@
 import styles from './navbar.module.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { GrFormClose } from 'react-icons/gr'
+import { useState } from 'react'
 
 const Navbar = () => {
+
+    const [showMenu, setShowMenu] = useState(true);
+
+    const toggleMenu = () => setShowMenu(!showMenu);
+
     return (
         <nav id={styles.nav}>
-            <h2>$ Welcome to my page, I'm <span style={{ 'color':  '#c300ff' }}>Aykut</span>.</h2>
-            <ul>
+            <h2>$ Welcome to my page, I'm <span style={{ 'color': '#c300ff' }}>Aykut</span>.</h2>
+            <ul className={`${showMenu ? styles.hide : styles.show}`} onClick={toggleMenu}>
+                <GrFormClose size={45} className={styles.closeMenu} />
                 <li><a href="/">Home</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#stack">Stack</a></li>
                 <li><a href="https://github.com/AykutSarac">GitHub</a></li>
             </ul>
+            <GiHamburgerMenu size={30} className={styles.menu} onClick={toggleMenu} />
         </nav>
     )
 }
