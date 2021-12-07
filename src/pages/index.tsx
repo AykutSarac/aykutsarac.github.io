c0fa094fbd699cd9ef421da85ee37a64192616a3
AykutSarac <aykutsarac0@gmail.com>
2021-12-07 15:03:44 +0300

    fetch blog posts & styling improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,39 +2,63 @@ import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Container from "components/Container";
 import Header from "containers/Header";
-import Navbar from "components/Navbar";
 import Projects from "containers/Projects";
-import Introduction from "containers/Introduction";
-import { Repository } from "constants/types";
+import { BlogPost, filteredRepos, Repository } from "constants/types";
+import Blog from "containers/Blog";
+import Skills from "containers/Skills";
+import Contact from "containers/Contact";
+import styled from "styled-components";
 
 interface Props {
   repos: Repository[];
+  blogPosts: BlogPost[];
 }
 
-const Home: NextPage<Props> = ({ repos }) => {
+const StyledContainer = styled(Container)`
+  padding: 60px 0 0;
+`;
+
+const Home: NextPage<Props> = ({ repos, blogPosts }) => {
   return (
     <div>
       <Head>
         <title>Aykut Saraç</title>
       </Head>
-      <Container>
-        <Navbar />
+      <StyledContainer>
         <Header />
-        <Introduction />
         <Projects repos={repos} />
-      </Container>
+        <Skills />
+        <Blog posts={blogPosts} />
+        <Contact />
+      </StyledContainer>
     </div>
   );
 };
 
+const sortByStar = (a: Repository, b: Repository) => {
+  if (!a.stargazers_count || !b.stargazers_count) return 0;
+
+  if (a.stargazers_count > b.stargazers_count) return -1;
+  return 1;
+};
+
+const filterRepos = (repo: Repository) => {
+  if (!filteredRepos.includes(repo.id)) return repo;
+};
 
 export const getStaticProps: GetStaticProps = async () => {
+  const medium = await fetch(
+    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@aykutsarac"
+  );
   const res = await fetch("https://api.github.com/users/aykutsarac/repos");
+  
   const repos = await res.json();
+  const blogPosts = await medium.json();
 
   return {
     props: {
-      repos,
+      repos: repos.sort(sortByStar).filter(filterRepos).slice(0, 5),
+      blogPosts: blogPosts.items.slice(0, 5),
     },
   };
 };
