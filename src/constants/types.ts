f9e21693bc2a767e027ed4257e60decbbe110a9b
AykutSarac <aykutsarac0@gmail.com>
2021-12-07 15:03:23 +0300

    create BlogPost interface

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -5,3 +5,16 @@ export interface Repository {
   description: string;
   stargazers_count?: number;
 }
+
+export interface BlogPost {
+  title: string;
+  pubDate: Date;
+  link: string;
+  guid: string;
+  description: string;
+  content: string;
+  categories: string[];
+  thumbnail: string;
+}
+
+export const filteredRepos = [345075300];
